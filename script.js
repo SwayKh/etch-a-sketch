1f0fba4bfe0b96e97f22a8cd2ead159b42bbf51f
SwayKh <khatrisumit712@gmail.com>
2023-10-14 23:06:37 +0530

    Done! Got the project working
    the logic and styles are Done
    might give some finishing touches later

--- script.js
+++ script.js
@@ -2,40 +2,51 @@ const grid = document.querySelector('.container')
 const resizeButton = document.querySelector('#resize-button')
 const clearButton = document.querySelector('#clear-button')
 
-let size = 16
+let defaultSize = 16
+let currentGridSize = defaultSize
+
+function renderGrid(noOfItems) {
+    for (let i = 1; i <= noOfItems ** 2; i++) {
+        const pixelDiv = document.createElement("div")
+        pixelDiv.className = "pixel"
+        pixelDiv.style.backgroundColor = "white"
+        pixelDiv.style.minWidth = String(grid.style.minWidth).replace("px", '') / noOfItems + "px"
+        pixelDiv.style.minHeight = String(grid.style.minHeight).replace("px", '') / noOfItems + "px"
+        grid.appendChild(pixelDiv)
+    }
+    currentGridSize = noOfItems
+}
+renderGrid(defaultSize)
+
+function resetGrid(existingSize) {
+    const pixel = document.querySelectorAll('.pixel')
+    pixel.forEach((element) => {
+        element.style.backgroundColor = "white"
+        grid.removeChild(element)
+    });
+    renderGrid(currentGridSize)
+}
 
 resizeButton.addEventListener('click', () => {
-    size = prompt("Size of the grid: ", "16")
-    while (size < 16 || size > 100) {
+    currentGridSize = prompt("Size of the grid: ", "16")
+
+    while (currentGridSize < 16 || currentGridSize > 100) {
         alert("Please enter a value above 16 and below 100")
-        size = prompt("Size of the grid: ", "16")
+        currentGridSize = prompt("Size of the grid: ", "16")
     }
-    console.log(size)
-    renderGrid(size)
+    resetGrid(currentGridSize)
 })
 
 clearButton.addEventListener('click', () => {
-    resetGrid()
+    resetGrid(currentGridSize)
 })
 
-function renderGrid(noOfItems) {
-    console.log("no of items: " + noOfItems)
-    console.log("container height: " + grid.style.minHeight)
-    resetGrid()
-    for (let i = 1; i <= size ** 2; i++) {
-        const pixelDiv = document.createElement("div")
-        pixelDiv.className = "pixel"
-        pixelDiv.style["min-width"] = grid.style.minWidth / noOfItems + "px"
-        pixelDiv.style["max-height"] = grid.style.minHeight / noOfItems + "px"
-        // pixelDiv.style.aspectRatio = "1/1"
-        grid.appendChild(pixelDiv)
-    }
-}
-
-function resetGrid() {
+grid.addEventListener('mouseenter', (e) => {
     const pixel = document.querySelectorAll('.pixel')
+
     pixel.forEach((element) => {
-        grid.removeChild(element)
-    });
-}
-renderGrid(25)
+        element.addEventListener("mouseover", () => {
+            element.style.backgroundColor = "black"
+        })
+    })
+})
