6cd674d76a0d8d43fac4ffa51d67feae15392ee6
SwayKh <khatrisumit712@gmail.com>
2023-10-14 21:52:24 +0530

    No idea what I changed, can't get anything to work

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
 const grid = document.querySelector('.container')
 const resizeButton = document.querySelector('#resize-button')
+const clearButton = document.querySelector('#clear-button')
 
-let size = 2
+let size = 16
 
 resizeButton.addEventListener('click', () => {
     size = prompt("Size of the grid: ", "16")
+    while (size < 16 || size > 100) {
+        alert("Please enter a value above 16 and below 100")
+        size = prompt("Size of the grid: ", "16")
+    }
     console.log(size)
-    renderGrid()
+    renderGrid(size)
 })
 
-function renderGrid() {
+clearButton.addEventListener('click', () => {
+    resetGrid()
+})
+
+function renderGrid(noOfItems) {
+    console.log("no of items: " + noOfItems)
+    console.log("container height: " + grid.style.minHeight)
     resetGrid()
     for (let i = 1; i <= size ** 2; i++) {
         const pixelDiv = document.createElement("div")
         pixelDiv.className = "pixel"
-        pixelDiv.textContent = i
-        pixelDiv.style.color = "white"
-        pixelDiv.style.backgroundColor = "#333333"
-        pixelDiv.style.textAlign = "center"
-        pixelDiv.style.minWidth = String(grid.style.minWidth / size) + "px"
-        pixelDiv.style.minHeight = String(grid.style.minHeight / size) + "px"
-        pixelDiv.style.aspectRatio = "1/1"
+        pixelDiv.style["min-width"] = grid.style.minWidth / noOfItems + "px"
+        pixelDiv.style["max-height"] = grid.style.minHeight / noOfItems + "px"
+        // pixelDiv.style.aspectRatio = "1/1"
         grid.appendChild(pixelDiv)
     }
 }
-renderGrid()
 
 function resetGrid() {
     const pixel = document.querySelectorAll('.pixel')
@@ -32,3 +38,4 @@ function resetGrid() {
         grid.removeChild(element)
     });
 }
+renderGrid(25)
